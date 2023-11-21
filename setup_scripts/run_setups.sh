ddaf08cd774f929470faf985c9e23982657f6edf
Shivan Khullar <khullar@tpb90.sunnyvale>
2023-11-21 18:26:19 -0500

    updated compilation flags to compile on starq

--- setup_scripts/run_setups.sh
+++ setup_scripts/run_setups.sh
@@ -1,11 +1,11 @@
 i=$PWD 
-for d in ../../* ; do
+for d in ../../sims/* ; do
     echo "$d"
-    #python clone_gizmo.py --repo_name=gizmo_imf_sk --dest_dir=$d
+    python clone_gizmo.py --repo_name=gizmo_imf_sk --dest_dir=$d
     cd $d/gizmo_imf_sk
     git reset --hard bc0ed35
     cd $i
-    python gizmo_setup.py --repo_dir=$d/gizmo_imf_sk/ --systype=nia
+    python gizmo_setup.py --repo_dir=$d/gizmo_imf_sk/
     #mkdir $d/output
     #touch $d/output/cpu.txt
     #cp ../cpu_performance_scripts/track_job.py $d/gizmo_imf_sk/
